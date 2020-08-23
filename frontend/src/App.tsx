a241f0408a59b6283bd59afc1f0bc67ddf4e19f2
Alan Bi <alan.bi326@gmail.com>
2020-08-22 17:48:19 -0700

    Set up eslint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,52 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
- 
-class App extends Component {
- 
-    state:any = {};
- 
-    componentDidMount() {
-        setInterval(this.hello, 250);
-    }
- 
-    hello = () => {
-        fetch('/hello')
-            .then(response => response.text())
-            .then(message => {
-                this.setState({message: message});
-            });
-    };
- 
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">{this.state.message}</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
-        );
-    }
+
+type MyProps = {};
+
+type MyState = {
+  message: String,
+};
+
+class App extends Component<MyProps, MyState> {
+
+  constructor(props: MyProps) {
+    super(props);
+
+    this.state = { message: 'Loading' };
+  }
+
+  componentDidMount() {
+    setInterval(this.hello, 250);
+  }
+
+  hello = () => {
+    fetch('/hello')
+      .then((response) => response.text())
+      .then((message) => {
+        this.setState({ message });
+      });
+  };
+
+  render() {
+    const { message } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">{message}</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
- 
+
 export default App;
