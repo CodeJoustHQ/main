e851789d34682e9286a19de7d6fc9c4b6b6efb06
Chris Elliott <elliott77chris@gmail.com>
2020-08-24 13:30:09 -0700

    Change User files to Problem.

---
+++ src/main/java/com/rocketden/main/dto/problem/Problem.java
@@ -0,0 +1,21 @@
+package com.rocketden.main.dto.problem;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Getter
+@Setter
+public class Problem {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+
+  private String name;
+	private String email;
+}
