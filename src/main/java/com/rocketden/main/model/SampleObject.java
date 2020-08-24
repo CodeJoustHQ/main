f252ba5c524294b2b92c731d7005a0279bbf8ae8
Chris Elliott <elliott77chris@gmail.com>
2020-08-24 11:41:05 -0700

    Set up connection to MySQL database through ClearDB.

--- src/main/java/com/rocketden/main/model/SampleObject.java
+++ src/main/java/com/rocketden/main/model/SampleObject.java
@@ -1,14 +1,14 @@
-package com.rocketden.main.model;
+// package com.rocketden.main.model;
 
-import lombok.Getter;
-import lombok.Setter;
+// import lombok.Getter;
+// import lombok.Setter;
 
-@Getter
-@Setter
-public class SampleObject {
-    private String message;
+// @Getter
+// @Setter
+// public class SampleObject {
+//     private String message;
 
-    public SampleObject(String message) {
-        this.message = message;
-    }
-}
+//     public SampleObject(String message) {
+//         this.message = message;
+//     }
+// }
