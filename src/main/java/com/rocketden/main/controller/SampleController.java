403d9506d50968ba5195907a74df27fea8f6f4c5
Chris Elliott <elliott77chris@gmail.com>
2020-08-24 12:08:27 -0700

    Add back the relevant dependencies.

--- src/main/java/com/rocketden/main/controller/SampleController.java
+++ src/main/java/com/rocketden/main/controller/SampleController.java
@@ -1,15 +1,15 @@
-// package com.rocketden.main.controller;
+package com.rocketden.main.controller;
 
-// import com.rocketden.main.model.SampleObject;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.RestController;
+import com.rocketden.main.model.SampleObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// public class SampleController {
+@RestController
+public class SampleController {
 
-//     @GetMapping("/hello")
-//     public SampleObject hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-//         return new SampleObject("Hello, " + name + "!");
-//     }
-// }
+    @GetMapping("/hello")
+    public SampleObject hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new SampleObject("Hello, " + name + "!");
+    }
+}
