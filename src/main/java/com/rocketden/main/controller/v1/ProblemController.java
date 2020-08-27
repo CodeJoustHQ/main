5694a6a39df360472e0fe1060cc791f796d633af
Chris Elliott <elliott77chris@gmail.com>
2020-08-27 13:20:27 -0700

    Fix spacing and remove unused function.

--- src/main/java/com/rocketden/main/controller/v1/ProblemController.java
+++ src/main/java/com/rocketden/main/controller/v1/ProblemController.java
@@ -47,26 +47,4 @@ public class ProblemController extends BaseRestController {
 		// Return a JSON with all problems in the database.
 		return problemRepository.findAll();
   }
-
-  /**
-   * Convert the Request Body in 'x-www-form-urlencoded' format to a HashMap 
-   * matching the keys to values.
-   * 
-   * @param bodyStr The passed-in String representing the Request Body.
-   * @return HashMap<String, String> that matches the body keys to values.
-   */
-  public Map<String, String> bodyToMap(String bodyStr) {
-    Map<String, String> body = new HashMap<>();
-
-    // Iterate through and parse out key-value pairs.
-    String[] values = bodyStr.split("&");
-    for (String value : values) {
-      String[] pair = value.split("=");
-      if (pair.length == 2) {
-        // Add the key-value pairs to the HashMap, replacing the '+' with space.
-        body.put(pair[0], pair[1].replace("+", " "));
-      }
-    }
-    return body;
-  }
 }
