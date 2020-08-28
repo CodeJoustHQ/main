7087cb310653313176051a502ac03118db1dc4b7
Alan Bi <alan.bi326@gmail.com>
2020-08-28 10:14:36 -0700

    Re-add an API file with the correct routes

---
+++ frontend/src/api/problem.ts
@@ -0,0 +1,13 @@
+type Problem = {
+  name: string;
+  description: string;
+}
+
+const basePath = '/api/v1/problems';
+const routes = {
+  getProblems: `${basePath}/`,
+  postProblem: `${basePath}/`,
+};
+
+export const getProblems = (): Promise<Problem> => fetch(routes.getProblems)
+  .then((response) => response.json());
