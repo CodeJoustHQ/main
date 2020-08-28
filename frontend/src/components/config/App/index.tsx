2bdd964d230b513cdc3fe4ed59258e635f6c6a25
Alan Bi <alan.bi326@gmail.com>
2020-08-28 09:57:47 -0700

    Change App to functional component and add rule to eslint

--- frontend/src/components/config/App/index.tsx
+++ frontend/src/components/config/App/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch } from 'react-router-dom';
 import MainLayout from '../../layout/Main';
 import LandingPage from '../../../views/Landing';
@@ -10,27 +10,17 @@ import CreateGamePage from '../../../views/Create';
 import JoinGamePage from '../../../views/Join';
 import GameResultsPage from '../../../views/Results';
 
-type MyProps = {};
-
-type MyState = {
-  message: String,
-};
-
-class App extends Component<MyProps, MyState> {
-  render() {
-    return (
-      <div>
-        <Switch>
-          <CustomRoute path="/" component={LandingPage} layout={MainLayout} exact />
-          <CustomRoute path="/game" component={GamePage} layout={GameLayout} exact />
-          <CustomRoute path="/game/create" component={CreateGamePage} layout={MainLayout} exact />
-          <CustomRoute path="/game/join" component={JoinGamePage} layout={MainLayout} exact />
-          <CustomRoute path="/game/results" component={GameResultsPage} layout={GameLayout} exact />
-          <CustomRoute path="*" component={NotFound} layout={MainLayout} />
-        </Switch>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Switch>
+      <CustomRoute path="/" component={LandingPage} layout={MainLayout} exact />
+      <CustomRoute path="/game" component={GamePage} layout={GameLayout} exact />
+      <CustomRoute path="/game/create" component={CreateGamePage} layout={MainLayout} exact />
+      <CustomRoute path="/game/join" component={JoinGamePage} layout={MainLayout} exact />
+      <CustomRoute path="/game/results" component={GameResultsPage} layout={GameLayout} exact />
+      <CustomRoute path="*" component={NotFound} layout={MainLayout} />
+    </Switch>
+  );
 }
 
 export default App;
