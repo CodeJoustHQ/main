8e6db7cdd0cb74a8d2fa84e54efd254bbe2b9c03
Chris Elliott <elliott77chris@gmail.com>
2020-09-01 08:00:44 -0700

    Remove unused function as no socket subscriptions are included yet.

--- src/main/java/com/rocketden/main/controller/v1/MessageController.java
+++ src/main/java/com/rocketden/main/controller/v1/MessageController.java
@@ -2,12 +2,9 @@ package com.rocketden.main.controller.v1;
 
 import com.rocketden.main.model.Message;
 
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.HtmlUtils;
 
 @RestController
 public class MessageController extends BaseRestController {
@@ -17,11 +14,4 @@ public class MessageController extends BaseRestController {
         return new Message("Hello, " + name + "!");
     }
 
-    // Are @MessageMapping and @SendTo both necessary?
-    @MessageMapping("/hello")
-	@SendTo("/topic/greetings")
-	public Message greeting(Message message) throws Exception {
-		Thread.sleep(1000); // simulated delay
-		return new Message("Hello, " + HtmlUtils.htmlEscape(message.getMessage()) + "!");
-	}
 }
