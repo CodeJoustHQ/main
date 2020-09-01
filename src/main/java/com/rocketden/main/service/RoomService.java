bc085645aabed6af0ef39a02185fe7b517c81e00
Alan Bi <alan.bi326@gmail.com>
2020-09-01 14:30:59 -0700

    Implement room service tests

--- src/main/java/com/rocketden/main/service/RoomService.java
+++ src/main/java/com/rocketden/main/service/RoomService.java
@@ -1,4 +1,25 @@
 package com.rocketden.main.service;
 
+import com.rocketden.main.dto.room.CreateRoomRequest;
+import com.rocketden.main.dto.room.CreateRoomResponse;
+import com.rocketden.main.dto.room.JoinRoomRequest;
+import com.rocketden.main.dto.room.JoinRoomResponse;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RoomService {
+
+    public static final int ROOM_ID_LENGTH = 6;
+
+    public JoinRoomResponse joinRoom(JoinRoomRequest request) {
+        return null;
+    }
+
+    public CreateRoomResponse createRoom(CreateRoomRequest request) {
+        return null;
+    }
+
+    protected String generateRoomId() {
+        return "";
+    }
 }
