629bf2c871a93d3c66ff5547f04a472a9afef03f
Chris Elliott <elliott77chris@gmail.com>
2020-09-09 21:14:25 -0700

    Update join page frontend file.

--- frontend/src/views/Join.tsx
+++ frontend/src/views/Join.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, ReactElement } from 'react';
 import { PrimaryButton } from '../components/core/Button';
+import { Text } from '../components/core/Text';
 import Input from '../components/core/Input';
 import { connect, disconnect, sendUser } from '../api/Socket';
 
@@ -10,20 +11,50 @@ function JoinGamePage() {
   // Declare nickname state variable.
   const [nickname, setNickname] = useState('');
 
-  return (
-    <div>
-      <Input placeholder="Enter your nickname" onChange={(event) => setNickname(event.target.value)} />
-      <PrimaryButton onClick={() => connect(SOCKET_ENDPOINT, nickname)}>
-        Connect
-      </PrimaryButton>
-      <PrimaryButton onClick={disconnect}>
-        Disconnect
-      </PrimaryButton>
-      <PrimaryButton onClick={() => sendUser(nickname)}>
-        Send User Information
-      </PrimaryButton>
-    </div>
-  );
+  /**
+   * Stores the current page state, where:
+   * 0 = Enter room ID state (currently unused)
+   * 1 = Enter nickname state
+   * 2 = Waiting room state
+   */
+  const [pageState, setPageState] = useState(1);
+
+  // Create variable to hold the "Join Page" content.
+  let joinPageContent: ReactElement | undefined;
+
+  switch (pageState) {
+    case 1:
+      // Render the "Enter nickname" state.
+      joinPageContent = (
+        <div>
+          <Input placeholder="Enter your nickname" onChange={(event) => setNickname(event.target.value)} />
+          <PrimaryButton
+            onClick={() => {
+              connect(SOCKET_ENDPOINT, nickname);
+              setPageState(1);
+            }}
+          >
+            Enter
+          </PrimaryButton>
+        </div>
+      );
+      break;
+    case 2:
+      // Render the Waiting room state.
+      joinPageContent = (
+        <div>
+          <Text>
+            You have entered the waiting room! Your nickname is &quot;
+            {nickname}
+            &quot;.
+          </Text>
+        </div>
+      );
+      break;
+    default:
+  }
+
+  return joinPageContent;
 }
 
 export default JoinGamePage;
