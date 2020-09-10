ef0d2b2ec6b438a91f58f9faffdf880ef6c887cf
Chris Elliott <elliott77chris@gmail.com>
2020-09-10 07:15:14 -0700

    Add protected constructor to BaseRestController class.

--- src/main/java/com/rocketden/main/controller/v1/BaseRestController.java
+++ src/main/java/com/rocketden/main/controller/v1/BaseRestController.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api/v1")
 public class BaseRestController {
 
+    /**
+     * Protected empty constructor to allow visability from extended classes,
+     * but also remove warning on Utility class having no constructor to hide
+     * the implicit public one.
+     */
+    protected BaseRestController() {}
+
     // Constants to hold custom paths.
     public static final String BASE_URL = "/api/v1";
     public static final String BASE_SOCKET_URL = "/api/v1/socket";
