4e06def72b98e7d53a2b37796e1ac63fedb988f6
Alan Bi <alan.bi326@gmail.com>
2020-09-11 18:59:59 -0700

    Move create page to landing page

--- frontend/src/components/config/App.tsx
+++ frontend/src/components/config/App.tsx
@@ -6,7 +6,6 @@ import NotFound from '../../views/NotFound';
 import { CustomRoute } from './Route';
 import GamePage from '../../views/Game';
 import GameLayout from '../layout/Game';
-import CreateGamePage from '../../views/Create';
 import JoinGamePage from '../../views/Join';
 import GameResultsPage from '../../views/Results';
 
@@ -15,7 +14,6 @@ function App() {
     <Switch>
       <CustomRoute path="/" component={LandingPage} layout={MainLayout} exact />
       <CustomRoute path="/game" component={GamePage} layout={GameLayout} exact />
-      <CustomRoute path="/game/create" component={CreateGamePage} layout={MainLayout} exact />
       <CustomRoute path="/game/join" component={JoinGamePage} layout={MainLayout} exact />
       <CustomRoute path="/game/results" component={GameResultsPage} layout={GameLayout} exact />
       <CustomRoute path="*" component={NotFound} layout={MainLayout} />
--- frontend/src/views/Landing.tsx
+++ frontend/src/views/Landing.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
-import { PrimaryButtonLink, TextLink } from '../components/core/Link';
+import React, { useCallback, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { TextButton } from '../components/core/Button';
+import { PrimaryButtonLink } from '../components/core/Link';
 import { LandingHeaderText } from '../components/core/Text';
+import { createRoom, Room } from '../api/Room';
+import { isError } from '../api/Error';
+import ErrorMessage from '../components/core/Error';
+import Loading from '../components/core/Loading';
 
 function LandingPage() {
+  const history = useHistory();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const createNewRoom = useCallback(() => {
+    const redirectToGame = (room: Room) => {
+      history.push('/game', { room });
+    };
+
+    setLoading(true);
+    createRoom()
+      .then((res) => {
+        setLoading(false);
+        if (isError(res)) {
+          setError(res.message);
+        } else {
+          redirectToGame(res as Room);
+        }
+      });
+  }, [history]);
+
   return (
     <div>
       <LandingHeaderText>
@@ -12,9 +39,11 @@ function LandingPage() {
         Join a Game
       </PrimaryButtonLink>
       <br />
-      <TextLink to="/game/create">
+      <TextButton onClick={createNewRoom}>
         Or create a room &#8594;
-      </TextLink>
+      </TextButton>
+      {error ? <ErrorMessage message={error} /> : null}
+      {loading ? <Loading /> : null}
     </div>
   );
 }
