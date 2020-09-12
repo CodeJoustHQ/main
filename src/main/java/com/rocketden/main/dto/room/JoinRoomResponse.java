71323d579734db5b9389a70fc730acbffc776876
Chris Elliott <elliott77chris@gmail.com>
2020-09-11 18:56:16 -0700

    Add user test and fix small response spelling error.

--- src/main/java/com/rocketden/main/dto/room/JoinRoomResponse.java
+++ src/main/java/com/rocketden/main/dto/room/JoinRoomResponse.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Setter
 public class JoinRoomResponse {
 
-    public static final String SUCCESS = "Sucessfully joined room.";
+    public static final String SUCCESS = "Successfully joined room.";
     public static final String ERROR_NOT_FOUND = "A room could not be found with the given id.";
     public static final String ERROR_USER_ALREADY_PRESENT = "A user with the features provided has already joined the room.";
     public static final String ERROR_NO_USER_FOUND = "No user was found when determining who is joining the database.";
---
+++ src/test/java/com/rocketden/main/UserTests.java
@@ -0,0 +1,52 @@
+package com.rocketden.main;
+
+import com.rocketden.main.dto.room.JoinRoomResponse;
+import com.rocketden.main.dto.user.CreateUserRequest;
+import com.rocketden.main.dto.user.CreateUserResponse;
+import com.rocketden.main.util.Utility;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(properties = "spring.datasource.type=com.zaxxer.hikari.HikariDataSource")
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@Transactional
+public class UserTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private static final String POST_USER = "/api/v1/user";
+
+    @Test
+    public void createNewUser() throws Exception {// PUT request to join non-existent room should fail
+        CreateUserRequest request = new CreateUserRequest();
+        request.setNickname("rocket");
+
+        CreateUserResponse expected = new CreateUserResponse();
+        expected.setMessage(CreateUserResponse.SUCCESS);
+
+        MvcResult result = this.mockMvc.perform(post(POST_USER)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(Utility.convertObjectToJsonString(request)))
+                .andDo(print()).andExpect(status().isCreated())
+                .andReturn();
+
+        String jsonResponse = result.getResponse().getContentAsString();
+        CreateUserResponse actual = Utility.toObject(jsonResponse, CreateUserResponse.class);
+
+        assertEquals(expected.getMessage(), actual.getMessage());
+    }
+}
