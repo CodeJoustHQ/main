172f472c89e93a2c196cfe0d568def8169fae3eb
Chris Elliott <elliott77chris@gmail.com>
2020-09-12 22:35:35 -0700

    Add EqualsAndHashCode annotation.

--- src/main/java/com/rocketden/main/model/User.java
+++ src/main/java/com/rocketden/main/model/User.java
@@ -5,44 +5,20 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@EqualsAndHashCode
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Exclude
     private Integer id;
     
     private String nickname;
-
-    // Overriding equals() to compare two users
-    @Override
-    public boolean equals(Object o) { 
-        // If the object is compared with itself then return true   
-        if (o == this) { 
-            return true; 
-        } 
-  
-        /* Check if o is an instance of User or not 
-          "null instanceof [type]" also returns false */
-        if (!(o instanceof User)) { 
-            return false; 
-        } 
-          
-        // Typecast o to User so that we can compare data members  
-        User user = (User) o; 
-          
-        // Compare the data members and return accordingly  
-        return this.getNickname().equals(user.getNickname());
-    } 
-
-    @Override
-    public int hashCode() {
-        return nickname.hashCode();
-    } 
-
 }
