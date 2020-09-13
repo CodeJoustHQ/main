04e55ec823f3803c115318434741e772ed74b658
Chris Elliott <elliott77chris@gmail.com>
2020-09-12 22:16:01 -0700

    Fix spacing.

--- src/main/java/com/rocketden/main/dto/user/CreateUserResponse.java
+++ src/main/java/com/rocketden/main/dto/user/CreateUserResponse.java
@@ -8,7 +8,9 @@ import lombok.Setter;
 public class CreateUserResponse {
     public static final String SUCCESS = "Successfully created user.";
     public static final String ERROR_NO_NICKNAME = "No nickname is provided for the user.";
-    public static final String ERROR_INVALID_NICKNAME = "The provided nickname" + " is invalid, for one of four reasons: it is empty, longer than sixteen" + " characters, or contains a space.";
+    public static final String ERROR_INVALID_NICKNAME = "The provided nickname" 
+        + " is invalid, for one of four reasons: it is empty, longer than" 
+        + " sixteen characters, or contains a space.";
 
     private String message;
     private String nickname;
