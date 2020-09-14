dcd9fa42446af9bd82326ae7ddd77a2ffdc0432f
Alan Bi <alan.bi326@gmail.com>
2020-09-14 13:20:42 -0700

    Add equals override to fix test cases

--- src/main/java/com/rocketden/main/exception/api/ApiError.java
+++ src/main/java/com/rocketden/main/exception/api/ApiError.java
@@ -1,10 +1,12 @@
 package com.rocketden.main.exception.api;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 // Error object holding response message and status details
 @Getter
+@EqualsAndHashCode
 public class ApiError {
     private final HttpStatus status;
     private final ApiErrorResponse response;
--- src/main/java/com/rocketden/main/exception/api/ApiErrorResponse.java
+++ src/main/java/com/rocketden/main/exception/api/ApiErrorResponse.java
@@ -1,9 +1,11 @@
 package com.rocketden.main.exception.api;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 // Response object returned to client containing message
 @Getter
+@EqualsAndHashCode
 public class ApiErrorResponse {
     private final String message;
 
