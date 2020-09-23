7617f938720e25af1df8df47126c027569050438
Chris Elliott <elliott77chris@gmail.com>
2020-09-22 21:53:31 -0700

    Create basic setup for the dual page state join page.

--- frontend/src/views/Join.tsx
+++ frontend/src/views/Join.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { ReactElement, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import EnterNicknamePage from '../components/core/EnterNickname';
 
 function JoinGamePage() {
   // Get history object to be able to move between different pages
   const history = useHistory();
+
+  /**
+   * Page state variable that defines whether the user is
+   * entering a room ID or a nickname.
+   * 1 = Enter Room ID
+   * 2 = Enter Nickname
+   */
+  const [pageState, setPageState] = useState(1);
 
   /**
    * Redirect the user to the lobby.
@@ -14,14 +22,62 @@ function JoinGamePage() {
     resolve();
   });
 
-  // Render the "Enter nickname" state.
-  return (
-    <EnterNicknamePage
-      enterNicknameHeaderText="Enter a nickname to join the game!"
-      // Partial application of addUserToLobby function.
-      enterNicknameAction={redirectToLobby}
-    />
-  );
+  let joinPageContent: ReactElement | undefined;
+
+  switch(pageState) {
+    case 1:
+      // Render the "Enter room ID" state.
+      joinPageContent = (
+        <LargeText>
+          {enterNicknameHeaderText}
+        </LargeText>
+        <LargeCenterInputText
+          placeholder="Your nickname"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setNickname(event.target.value);
+          }}
+          onFocus={() => {
+            setFocusInput(true);
+          }}
+          onBlur={() => {
+            setFocusInput(false);
+          }}
+          onKeyPress={(event) => {
+            if (event.key === 'Enter' && validNickname) {
+              enterNicknameActionUpdatePage(nickname);
+            }
+          }}
+        />
+        <LargeInputButton
+          onClick={() => {
+            enterNicknameActionUpdatePage(nickname);
+          }}
+          value="Enter"
+          // Input is disabled if no nickname exists, has a space, or is too long.
+          disabled={!validNickname}
+        />
+        { focusInput && !validNickname ? (
+          <Text>
+            The nickname must be non-empty, have no spaces, and be less than 16 characters.
+          </Text>
+        ) : null}
+        { loading ? <Loading /> : null }
+        { error ? <ErrorMessage message={error} /> : null }
+      );
+    case 2: 
+      // Render the "Enter nickname" state.
+      joinPageContent = (
+        <EnterNicknamePage
+          enterNicknameHeaderText="Enter a nickname to join the game!"
+          // Partial application of addUserToLobby function.
+          enterNicknameAction={redirectToLobby}
+        />
+      );
+    default: setPageState(1);
+  }
+  
+
+  return joinPageContent;
 }
 
 export default JoinGamePage;
