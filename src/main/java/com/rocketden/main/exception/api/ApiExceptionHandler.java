459ac540883b9018b5c0e3ff538fc98c497c25d5
Alan Bi <alan.bi326@gmail.com>
2020-10-07 15:21:57 -0700

    Fix issue with conflicting exception types

--- src/main/java/com/rocketden/main/exception/api/ApiExceptionHandler.java
+++ src/main/java/com/rocketden/main/exception/api/ApiExceptionHandler.java
@@ -1,8 +1,13 @@
 package com.rocketden.main.exception.api;
 
+import com.rocketden.main.exception.RoomError;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -12,6 +17,15 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ApiException.class)
     protected ResponseEntity<ApiErrorResponse> handleApiException(ApiException e) {
         ApiError apiError = e.getError();
+        return new ResponseEntity<>(apiError.getResponse(), apiError.getStatus());
+    }
+
+    // Handle failure to convert from string to enum (e.g. "easy" -> Difficulty.EASY)
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
+        ApiError apiError = RoomError.BAD_SETTING;
         return new ResponseEntity<>(apiError.getResponse(), apiError.getStatus());
     }
 }
--- src/main/java/com/rocketden/main/model/Difficulty.java
+++ src/main/java/com/rocketden/main/model/Difficulty.java
@@ -1,8 +1,6 @@
 package com.rocketden.main.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.rocketden.main.exception.RoomError;
-import com.rocketden.main.exception.api.ApiException;
 
 public enum Difficulty {
     EASY, MEDIUM, HARD;
@@ -10,10 +8,6 @@ public enum Difficulty {
     // Convert a matching string (ignoring case) to enum object
     @JsonCreator
     public static Difficulty fromString(String value) {
-        try {
-            return Difficulty.valueOf(value.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            throw new ApiException(RoomError.BAD_SETTING);
-        }
+        return Difficulty.valueOf(value.toUpperCase());
     }
 }
