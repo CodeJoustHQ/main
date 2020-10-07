61c3a59e2237358311feea5d073f8157e7e45dd9
Alan Bi <alan.bi326@gmail.com>
2020-10-07 15:00:20 -0700

    Create mapping from string to difficulty enum

--- src/main/java/com/rocketden/main/model/Difficulty.java
+++ src/main/java/com/rocketden/main/model/Difficulty.java
@@ -1,5 +1,19 @@
 package com.rocketden.main.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.rocketden.main.exception.RoomError;
+import com.rocketden.main.exception.api.ApiException;
+
 public enum Difficulty {
-    EASY, MEDIUM, HARD
+    EASY, MEDIUM, HARD;
+
+    // Convert a matching string (ignoring case) to enum object
+    @JsonCreator
+    public static Difficulty fromString(String value) {
+        try {
+            return Difficulty.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new ApiException(RoomError.BAD_SETTING);
+        }
+    }
 }
