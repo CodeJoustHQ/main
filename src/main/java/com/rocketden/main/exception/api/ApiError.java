2cd457b24a6ad11cf43a1ef59ca2eddf1e3c4a54
Chris Elliott <elliott77chris@gmail.com>
2020-10-07 21:32:14 -0700

    Resolve errors on ApiException and ApiError.

--- src/main/java/com/rocketden/main/exception/api/ApiError.java
+++ src/main/java/com/rocketden/main/exception/api/ApiError.java
@@ -1,9 +1,11 @@
 package com.rocketden.main.exception.api;
 
+import java.io.Serializable;
+
 import org.springframework.http.HttpStatus;
 
 // Interface representing an API error that holds status and response object
-public interface ApiError {
+public interface ApiError extends Serializable {
 
     HttpStatus getStatus();
 
--- src/main/java/com/rocketden/main/exception/api/ApiException.java
+++ src/main/java/com/rocketden/main/exception/api/ApiException.java
@@ -5,6 +5,10 @@ import lombok.Getter;
 // Exception used to trigger API error responses
 @Getter
 public class ApiException extends RuntimeException {
+    /**
+     * Hard-coded serial version ID to avoid warning.
+     */
+    private static final long serialVersionUID = 1L;
     private final ApiError error;
 
     public ApiException(ApiError error) {
