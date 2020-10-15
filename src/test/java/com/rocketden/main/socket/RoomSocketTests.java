e2a932b37f979cf420eacb34cb5e4917f62ff0f4
Alan Bi <alan.bi326@gmail.com>
2020-10-15 15:46:16 -0700

    Add testing for second user connecting

--- src/test/java/com/rocketden/main/socket/RoomSocketTests.java
+++ src/test/java/com/rocketden/main/socket/RoomSocketTests.java
@@ -85,7 +85,7 @@ public class RoomSocketTests {
 
         // Next, set up the socket connection and subscription
         // BlockingQueue will hold the responses from the socket subscribe endpoint
-        blockingQueue = new ArrayBlockingQueue<>(1);
+        blockingQueue = new ArrayBlockingQueue<>(2);
 
         StompHeaders headers = new StompHeaders();
         headers.add("userId", host.getUserId());
@@ -201,9 +201,43 @@ public class RoomSocketTests {
         assertNull(room.getHost().getSessionId());
 
         // Get the room to verify that the sessionId has been saved in the database
-        RoomDto actual = template.getForObject(String.format(baseRestEndpoint, room.getRoomId()), RoomDto.class);
+        String roomEndpoint = String.format("%s/%s", baseRestEndpoint, room.getRoomId());
+        RoomDto actual = template.getForObject(roomEndpoint, RoomDto.class);
 
         assertNotNull(actual);
         assertNotNull(actual.getHost().getSessionId());
+
+        // Have someone else join the room
+        UserDto user = new UserDto();
+        user.setNickname("test");
+        JoinRoomRequest joinRequest = new JoinRoomRequest();
+        joinRequest.setUser(user);
+
+        HttpEntity<JoinRoomRequest> joinEntity = new HttpEntity<>(joinRequest);
+        String joinRoomEndpoint = String.format("%s/users", roomEndpoint);
+        template.exchange(joinRoomEndpoint, HttpMethod.PUT, joinEntity, RoomDto.class).getBody();
+
+        // Initially, the new user is not connected, so their sessionId should be null
+        actual = blockingQueue.poll(3, SECONDS);
+        assertNotNull(actual);
+
+        user = actual.getUsers().get(1);
+        assertNull(user.getSessionId());
+
+        // The user then connects to the stomp client
+        WebSocketStompClient newStompClient = new WebSocketStompClient(new SockJsClient(
+                List.of(new WebSocketTransport(new StandardWebSocketClient()))));
+        newStompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        StompHeaders headers = new StompHeaders();
+        headers.add("userId", user.getUserId());
+
+        stompClient.connect(CONNECT_ENDPOINT, new WebSocketHttpHeaders(), headers, new StompSessionHandlerAdapter() {}, this.port)
+                .get(3, SECONDS);
+
+        // After connecting, the new user's sessionId should no longer be null
+        actual = blockingQueue.poll(3, SECONDS);
+        assertNotNull(actual);
+        assertNotNull(actual.getUsers().get(1).getSessionId());
     }
 }
