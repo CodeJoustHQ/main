ef4323587c75234957dda30a54d1121027282854
Chris Elliott <elliott77chris@gmail.com>
2020-10-16 09:32:00 -0700

    Reformat room entity tests to include constants.

--- src/test/java/com/rocketden/main/entity/RoomEntityTests.java
+++ src/test/java/com/rocketden/main/entity/RoomEntityTests.java
@@ -14,6 +14,12 @@ import static org.junit.Assert.assertTrue;
 @SpringBootTest
 public class RoomEntityTests {
 
+    // Predefine user attributes.
+    private static final String NICKNAME = "rocket";
+    private static final String NICKNAME_2 = "rocketrocket";
+    private static final String USER_ID = "012345";
+    private static final String USER_ID_2 = "012345";
+
     @Test
     public void roomInitialization() {
         Room room = new Room();
@@ -28,7 +34,7 @@ public class RoomEntityTests {
         Room room = new Room();
 
         User user = new User();
-        user.setNickname("test");
+        user.setNickname(NICKNAME);
 
         room.addUser(user);
 
@@ -41,19 +47,18 @@ public class RoomEntityTests {
         Room room = new Room();
 
         User user = new User();
-        user.setNickname("test");
-        user.setId(1);
+        user.setNickname(NICKNAME);
+        user.setUserId(USER_ID);
         room.addUser(user);
         
         User userToRemove = new User();
-        userToRemove.setNickname("nonexistent");
-        userToRemove.setId(2);
+        userToRemove.setNickname(NICKNAME_2);
+        userToRemove.setUserId(USER_ID_2);
 
         assertFalse(room.removeUser(userToRemove));
         assertTrue(room.getUsers().contains(user));
 
-        userToRemove.setNickname("test");
-        assertTrue(room.removeUser(userToRemove));
+        assertTrue(room.removeUser(user));
         assertFalse(room.getUsers().contains(user));
     }
 
@@ -62,21 +67,19 @@ public class RoomEntityTests {
         Room room = new Room();
 
         User user = new User();
-        user.setNickname("test");
-        user.setId(1);
+        user.setNickname(NICKNAME);
+        user.setUserId(USER_ID);
         room.addUser(user);
 
         User userToGet = new User();
-        userToGet.setNickname("nonexistent");
-        userToGet.setId(2);
+        userToGet.setNickname(NICKNAME_2);
+        userToGet.setUserId(USER_ID_2);
 
         assertNull(room.getEquivalentUser(userToGet));
 
-        userToGet.setNickname("test");
-
-        User actual = room.getEquivalentUser(userToGet);
+        User actual = room.getEquivalentUser(user);
 
         assertEquals(user.getNickname(), actual.getNickname());
-        assertEquals(user.getId(), actual.getId());
+        assertEquals(user.getUserId(), actual.getUserId());
     }
 }
