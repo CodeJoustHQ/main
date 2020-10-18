acc2049edf891cd7e11b3689e0022183c70fa9af
Alan Bi <alan.bi326@gmail.com>
2020-10-18 15:36:00 -0700

    Create unit tests for problem service

---
+++ src/test/java/com/rocketden/main/service/ProblemServiceTests.java
@@ -0,0 +1,65 @@
+package com.rocketden.main.service;
+
+import com.rocketden.main.dao.ProblemRepository;
+import com.rocketden.main.dto.problem.CreateProblemRequest;
+import com.rocketden.main.dto.problem.ProblemDto;
+import com.rocketden.main.model.Problem;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class ProblemServiceTests {
+
+    @Mock
+    private ProblemRepository repository;
+
+    @Spy
+    @InjectMocks
+    private ProblemService problemService;
+
+    private static final String NAME = "Sort a List";
+    private static final String DESCRIPTION = "Sort the given list in O(n log n) time.";
+
+    @Test
+    public void createProblemSuccess() {
+        CreateProblemRequest request = new CreateProblemRequest();
+        request.setName(NAME);
+        request.setDescription(DESCRIPTION);
+
+        ProblemDto response = problemService.createProblem(request);
+
+        verify(repository).save(Mockito.any(Problem.class));
+
+        assertEquals(NAME, response.getName());
+        assertEquals(DESCRIPTION, response.getDescription());
+    }
+
+    @Test
+    public void getProblemsSuccess() {
+        Problem problem = new Problem();
+        problem.setName(NAME);
+        problem.setDescription(DESCRIPTION);
+
+        List<Problem> expected = new ArrayList<>();
+        expected.add(problem);
+
+        Mockito.doReturn(expected).when(repository).findAll();
+
+        List<ProblemDto> response = problemService.getAllProblems();
+
+        assertEquals(1, response.size());
+        assertEquals(NAME, response.get(0).getName());
+        assertEquals(DESCRIPTION, response.get(1).getDescription());
+    }
+}
