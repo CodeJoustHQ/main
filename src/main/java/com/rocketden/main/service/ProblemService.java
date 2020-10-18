1c44d6e73884e8198254c20e3800a1fee4e30abf
Alan Bi <alan.bi326@gmail.com>
2020-10-18 15:47:53 -0700

    Add problem error validation

--- src/main/java/com/rocketden/main/service/ProblemService.java
+++ src/main/java/com/rocketden/main/service/ProblemService.java
@@ -4,6 +4,8 @@ import com.rocketden.main.dao.ProblemRepository;
 import com.rocketden.main.dto.problem.CreateProblemRequest;
 import com.rocketden.main.dto.problem.ProblemDto;
 import com.rocketden.main.dto.problem.ProblemMapper;
+import com.rocketden.main.exception.ProblemError;
+import com.rocketden.main.exception.api.ApiException;
 import com.rocketden.main.model.Problem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,10 @@ public class ProblemService {
     }
 
     public ProblemDto createProblem(CreateProblemRequest request) {
+        if (request.getName() == null || request.getDescription() == null) {
+            throw new ApiException(ProblemError.EMPTY_FIELD);
+        }
+
         Problem problem = new Problem();
         problem.setName(request.getName());
         problem.setDescription(request.getDescription());
--- src/test/java/com/rocketden/main/api/ProblemTests.java
+++ src/test/java/com/rocketden/main/api/ProblemTests.java
@@ -9,10 +9,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.rocketden.main.dto.problem.CreateProblemRequest;
 import com.rocketden.main.dto.problem.ProblemDto;
+import com.rocketden.main.exception.ProblemError;
+import com.rocketden.main.exception.api.ApiError;
+import com.rocketden.main.exception.api.ApiErrorResponse;
 import com.rocketden.main.util.UtilityTestMethods;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -33,51 +36,53 @@ import java.util.List;
 @Transactional
 class ProblemTests {
 
-    private static ProblemDto problem1;
-    private static ProblemDto problem2;
-
     @Autowired
     private MockMvc mockMvc;
 
     private static final String GET_PROBLEM_ALL = "/api/v1/problems";
     private static final String POST_PROBLEM_CREATE = "/api/v1/problems";
 
-    @BeforeEach
-    public void setup() {
-        problem1 = new ProblemDto();
-        problem1.setName("Sort an Array");
-        problem1.setDescription("Sort an array from lowest to highest value.");
-
-        problem2 = new ProblemDto();
-        problem2.setName("Find Maximum");
-        problem2.setDescription("Find the maximum value in an array.");
-    }
+    private static final String NAME = "Sort an Array";
+    private static final String DESCRIPTION = "Sort an array from lowest to highest value.";
+    private static final String NAME_2 = "Find Maximum";
+    private static final String DESCRIPTION_2 = "Find the maximum value in an array.";
 
     @Test
     public void createProblemSuccess() throws Exception {
+        CreateProblemRequest request = new CreateProblemRequest();
+        request.setName(NAME);
+        request.setName(DESCRIPTION);
+
         MvcResult result = this.mockMvc.perform(post(POST_PROBLEM_CREATE)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(UtilityTestMethods.convertObjectToJsonString(problem1)))
+                .content(UtilityTestMethods.convertObjectToJsonString(request)))
                 .andDo(print()).andExpect(status().isCreated())
                 .andReturn();
 
         String jsonResponse = result.getResponse().getContentAsString();
         ProblemDto actual = UtilityTestMethods.toObject(jsonResponse, ProblemDto.class);
 
-        assertEquals(problem1.getName(), actual.getName());
-        assertEquals(problem1.getDescription(), actual.getDescription());
+        assertEquals(NAME, actual.getName());
+        assertEquals(DESCRIPTION, actual.getDescription());
     }
 
     @Test
     public void createProblemsAndGetProblems() throws Exception {
-        this.mockMvc.perform(post(POST_PROBLEM_CREATE)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(UtilityTestMethods.convertObjectToJsonString(problem1)))
-                .andDo(print()).andExpect(status().isCreated());
+        CreateProblemRequest request = new CreateProblemRequest();
+        request.setName(NAME);
+        request.setName(DESCRIPTION);
 
         this.mockMvc.perform(post(POST_PROBLEM_CREATE)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(UtilityTestMethods.convertObjectToJsonString(problem2)))
+                .content(UtilityTestMethods.convertObjectToJsonString(request)))
+                .andDo(print()).andExpect(status().isCreated());
+
+        request.setName(NAME_2);
+        request.setName(DESCRIPTION_2);
+
+        this.mockMvc.perform(post(POST_PROBLEM_CREATE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(UtilityTestMethods.convertObjectToJsonString(request)))
                 .andDo(print()).andExpect(status().isCreated());
 
         // After creating two problems, check that the GET request finds them all
@@ -91,11 +96,30 @@ class ProblemTests {
         List<ProblemDto> actual = new Gson().fromJson(jsonResponse, listType);
 
         assertEquals(2, actual.size());
-        assertEquals(problem1.getName(), actual.get(0).getName());
-        assertEquals(problem1.getDescription(), actual.get(0).getDescription());
+        assertEquals(NAME, actual.get(0).getName());
+        assertEquals(DESCRIPTION, actual.get(0).getDescription());
 
-        assertEquals(problem2.getName(), actual.get(1).getName());
-        assertEquals(problem2.getDescription(), actual.get(1).getDescription());
+        assertEquals(NAME_2, actual.get(1).getName());
+        assertEquals(DESCRIPTION_2, actual.get(1).getDescription());
+    }
+
+    @Test
+    public void createProblemEmptyFields() throws Exception {
+        CreateProblemRequest request = new CreateProblemRequest();
+        request.setName(NAME);
+
+        ApiError ERROR = ProblemError.EMPTY_FIELD;
+
+        MvcResult result = this.mockMvc.perform(post(POST_PROBLEM_CREATE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(UtilityTestMethods.convertObjectToJsonString(request)))
+                .andDo(print()).andExpect(status().is(ERROR.getStatus().value()))
+                .andReturn();
+
+        String jsonResponse = result.getResponse().getContentAsString();
+        ApiErrorResponse actual = UtilityTestMethods.toObject(jsonResponse, ApiErrorResponse.class);
+
+        assertEquals(ERROR.getResponse(), actual);
     }
 
     @Test
