1fec571e862dc21ef5cd306e95628a13fc39d3d0
Chris Elliott <elliott77chris@gmail.com>
2020-10-21 09:03:23 -0700

    Remove unused imports.

--- src/test/java/com/rocketden/main/util/UtilityTests.java
+++ src/test/java/com/rocketden/main/util/UtilityTests.java
@@ -13,8 +13,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
