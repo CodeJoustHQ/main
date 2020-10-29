1f44ad65dffc5982c4b55f0ba5a3954f362de521
Chris Elliott <elliott77chris@gmail.com>
2020-10-28 21:37:20 -0700

    Remove unused imports from GameManagementService.

--- src/main/java/com/rocketden/main/service/GameManagementService.java
+++ src/main/java/com/rocketden/main/service/GameManagementService.java
@@ -1,3 +1,18 @@
+package com.rocketden.main.service;
+
+import com.rocketden.main.dao.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GameManagementService {
-    
+
+    private final RoomRepository repository;
+	private final SocketService socketService;
+
+    @Autowired
+    public GameService(RoomRepository repository, SocketService socketService) {
+        this.repository = repository;
+        this.socketService = socketService;
+    }
 }
