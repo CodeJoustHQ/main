9ecc7b83f619378a1dae55e4ec714225d9a43501
Chris Elliott <elliott77chris@gmail.com>
2020-11-03 09:37:16 -0800

    Add submit service and game management service.

--- src/main/java/com/rocketden/main/service/GameManagementService.java
+++ src/main/java/com/rocketden/main/service/GameManagementService.java
@@ -1,19 +1,44 @@
 package com.rocketden.main.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.rocketden.main.dao.RoomRepository;
+import com.rocketden.main.model.Game;
+import com.rocketden.main.model.Room;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GameManagementService {
+public abstract class GameManagementService {
 
     private final RoomRepository repository;
-	private final SocketService socketService;
+    private final SocketService socketService;
+    private Map<String, Game> currentGameMap;
 
     @Autowired
-    public GameManagementService(RoomRepository repository, SocketService socketService) {
+    protected GameManagementService(RoomRepository repository, SocketService socketService) {
         this.repository = repository;
         this.socketService = socketService;
+        currentGameMap = new HashMap<>();
+    }
+
+    public Game createGameFromRoom(Room room) {
+        // TODO: Create the game from the room (or roomId).
+        return null;
+    }
+
+    public void addGame(Game game) {
+        currentGameMap.put(game.getRoomId(), game);
+    }
+
+    protected Game getGameFromRoomId(String roomId) {
+        return currentGameMap.get(roomId);
+    }
+
+    protected void removeGame(String roomId) {
+        currentGameMap.remove(roomId);
     }
 
 }
---
+++ src/main/java/com/rocketden/main/service/SubmitService.java
@@ -0,0 +1,16 @@
+package com.rocketden.main.service;
+
+import com.rocketden.main.dao.RoomRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * Class to handle code updates and miscellaneous requests.
+ */
+@Service
+public class SubmitService extends GameManagementService {
+
+    public SubmitService(RoomRepository repository, SocketService socketService) {
+        super(repository, socketService);
+    }
+
+}
