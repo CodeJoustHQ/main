5b3266b2772cfc23f8775b650ee72edeabd089a2
Chris Elliott <elliott77chris@gmail.com>
2020-12-27 17:15:36 -0800

    Remove unused import.

--- src/test/java/com/rocketden/main/service/ProblemServiceTests.java
+++ src/test/java/com/rocketden/main/service/ProblemServiceTests.java
@@ -20,7 +20,6 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
