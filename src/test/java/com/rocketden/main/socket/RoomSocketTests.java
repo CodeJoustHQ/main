fa5d470a1885c0f994a4b1432afcb181b539a3bd
Chris Elliott <elliott77chris@gmail.com>
2021-01-08 16:29:58 -0800

    Change assert to jupiter import.

--- src/test/java/com/rocketden/main/socket/RoomSocketTests.java
+++ src/test/java/com/rocketden/main/socket/RoomSocketTests.java
@@ -38,7 +38,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
