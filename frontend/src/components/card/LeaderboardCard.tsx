e03c22b133583f7d611a2c1b2ada7c31e9d996f6
Alan Bi <alan.bi326@gmail.com>
2021-01-08 22:14:10 -0800

    Create leaderboard card

---
+++ frontend/src/components/card/LeaderboardCard.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Player } from '../../api/Game';
+import { Text } from '../core/Text';
+
+const Content = styled.div`
+  margin: 10px;
+`;
+
+const PlayerIcon = styled.div`
+  background-color: ${({ theme }) => theme.colors.blue};
+  border-radius: 50%;
+`;
+
+type LeaderboardCardProps = {
+  player: Player,
+  isCurrentPlayer: boolean,
+};
+
+function LeaderboardCard(props: LeaderboardCardProps) {
+  const { player, isCurrentPlayer } = props;
+
+  return (
+    <Content>
+      <PlayerIcon>
+        <Text>{player.user.nickname}</Text>
+        <Text>{isCurrentPlayer ? ' (you)' : ''}</Text>
+      </PlayerIcon>
+
+      <Text>
+        Solved:
+        {player.solved}
+      </Text>
+    </Content>
+  );
+}
+
+export default LeaderboardCard;
