aa6f9f4f23f3dbfaaa11e783080bec1233affea8
Chris Elliott <elliott77chris@gmail.com>
2021-01-10 08:49:02 -0800

    Move constructor below all fields in GameTimer.java.

--- src/main/java/com/rocketden/main/game_object/GameTimer.java
+++ src/main/java/com/rocketden/main/game_object/GameTimer.java
@@ -18,18 +18,6 @@ public class GameTimer {
     // 15 minute duration for the GameTimer, in seconds.
     public static final Long DURATION_15 = (long) 900;
 
-    /**
-     * Instantiate the GameTimer class, and schedule the end game task after
-     * timer ends.
-     * 
-     * @param duration The duration, in seconds, until the timer ends.
-     */
-    public GameTimer(Long duration) {
-        this.duration = duration;
-        this.endTime = this.startTime.plusSeconds(duration);
-        this.timer = new Timer();
-    }
-
     // The time that the game began.
     private LocalDateTime startTime = LocalDateTime.now();
 
@@ -41,5 +29,17 @@ public class GameTimer {
     private boolean timeUp = false;
 
     private Timer timer;
+
+    /**
+     * Instantiate the GameTimer class, and schedule the end game task after
+     * timer ends.
+     * 
+     * @param duration The duration, in seconds, until the timer ends.
+     */
+    public GameTimer(Long duration) {
+        this.duration = duration;
+        this.endTime = this.startTime.plusSeconds(duration);
+        this.timer = new Timer();
+    }
 
 }
