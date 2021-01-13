f4ec3a85748e68544fd7c8d802e6fa6ec2240fbf
Alan Bi <alan.bi326@gmail.com>
2021-01-12 23:25:44 -0800

    Add more content to results player cards

--- frontend/src/components/card/PlayerResultsCard.tsx
+++ frontend/src/components/card/PlayerResultsCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Player } from '../../api/Game';
+import { MediumText, Text } from '../core/Text';
 
 const Content = styled.div`
   display: block;
@@ -15,11 +16,19 @@ type PlayerResultsCardProps = {
 function PlayerResultsCard(props: PlayerResultsCardProps) {
   const { player, place } = props;
 
+  const latestSubmission = player.submissions.slice(-1)[0];
+  let status = '';
+  if (!latestSubmission) {
+    status = 'No attempts';
+  } else {
+    status = `${latestSubmission.numCorrect} / ${latestSubmission.numTestCases}`;
+    // TODO: include time and color
+  }
+
   return (
     <Content>
-      {place}
-      .
-      {player.user.nickname}
+      <MediumText>{`${place}. ${player.user.nickname}`}</MediumText>
+      <Text>{status}</Text>
     </Content>
   );
 }
