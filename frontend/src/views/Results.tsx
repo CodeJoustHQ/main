8b944bc5b9a03da9aadfa6cabaa16b5d5c1949ca
Alan Bi <alan.bi326@gmail.com>
2021-01-12 22:56:13 -0800

    Accept game from location state or redirect

--- frontend/src/views/Results.tsx
+++ frontend/src/views/Results.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
 import { LargeText } from '../components/core/Text';
-import { Game } from '../api/Game';
+import { Game, Player } from '../api/Game';
+import { checkLocationState } from '../util/Utility';
+import { errorHandler } from '../api/Error';
 
 type LocationState = {
   game: Game,
 };
 
 function GameResultsPage() {
+  const history = useHistory();
   const location = useLocation<LocationState>();
+  const [players, setPlayers] = useState<Player[]>();
+
+  useEffect(() => {
+    if (checkLocationState(location, 'game')) {
+      setPlayers(location.state.game.players);
+    } else {
+      history.replace('/game/join', {
+        error: errorHandler('Please join and play a game before viewing the results page.'),
+      });
+    }
+  }, [location, history]);
 
   return (
     <div>
