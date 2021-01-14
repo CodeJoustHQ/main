fe1f4cb7ae07f499bdb99611cdb4ee28731f2f88
Alan Bi <alan.bi326@gmail.com>
2021-01-13 23:14:53 -0800

    Change sorting to use DTOs to avoid issue with maps

--- src/main/java/com/rocketden/main/service/SubmitService.java
+++ src/main/java/com/rocketden/main/service/SubmitService.java
@@ -1,6 +1,8 @@
 package com.rocketden.main.service;
 
+import com.rocketden.main.dto.game.GameDto;
 import com.rocketden.main.dto.game.GameMapper;
+import com.rocketden.main.dto.game.PlayerDto;
 import com.rocketden.main.dto.game.SubmissionDto;
 import com.rocketden.main.dto.game.SubmissionRequest;
 import com.rocketden.main.game_object.Game;
@@ -11,7 +13,6 @@ import com.rocketden.main.game_object.Submission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -49,41 +50,40 @@ public class SubmitService {
         player.setSolved(true);
 
         // Sort list of players by who is winning
-        sortLeaderboard(game.getPlayers());
+        GameDto gameDto = GameMapper.toDto(game);
+        sortLeaderboard(gameDto.getPlayers());
 
         // Send socket update with latest leaderboard info
-        socketService.sendSocketUpdate(GameMapper.toDto(game));
         // TODO: test socket in GameSocketTests when merged
+        socketService.sendSocketUpdate(gameDto);
 
         return GameMapper.submissionToDto(submission);
     }
 
     // Sort by numCorrect followed by startTime
-    public void sortLeaderboard(List<Player> players) {
-        Collections.sort(players, new Comparator<Player>() {
-            int compare(Player player1, Player player2)  {
-                List<Submission> submissions1 = player1.getSubmissions();
-                List<Submission> submissions2 = player2.getSubmissions();
+    public void sortLeaderboard(List<PlayerDto> players) {
+        players.sort((player1, player2) -> {
+            List<SubmissionDto> submissions1 = player1.getSubmissions();
+            List<SubmissionDto> submissions2 = player2.getSubmissions();
 
-                // Players who haven't submitted yet are sorted last
-                if (submissions1.isEmpty()) {
-                    return 1;
-                }
-                if (submissions2.isEmpty()) {
-                    return -1;
-                }
-
-                Submission sub1 = submissions1.get(submissions1.size() - 1);
-                Submission sub2 = submissions2.get(submissions2.size() - 1);
-
-                // If both have the same numCorrect, whoever submits earlier is first
-                if (sub1.getNumCorrect().equals(sub2.getNumCorrect())) {
-                    return sub1.getStartTime().compareTo(sub2.getStartTime());
-                }
-
-                // Whoever has higher numCorrect is first
-                return sub2.getNumCorrect() - sub1.getNumCorrect();
+            // Players who haven't submitted yet are sorted last
+            if (submissions1.isEmpty()) {
+                return 1;
             }
+            if (submissions2.isEmpty()) {
+                return -1;
+            }
+
+            SubmissionDto sub1 = submissions1.get(submissions1.size() - 1);
+            SubmissionDto sub2 = submissions2.get(submissions2.size() - 1);
+
+            // If both have the same numCorrect, whoever submits earlier is first
+            if (sub1.getNumCorrect().equals(sub2.getNumCorrect())) {
+                return sub1.getStartTime().compareTo(sub2.getStartTime());
+            }
+
+            // Whoever has higher numCorrect is first
+            return sub2.getNumCorrect() - sub1.getNumCorrect();
         });
     }
 }
