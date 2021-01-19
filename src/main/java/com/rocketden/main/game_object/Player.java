778c85e5e2d799f377f21146a4d757c43833e19e
Chris Elliott <elliott77chris@gmail.com>
2021-01-18 19:16:17 -0800

    Add tests for updateCode funtion in service.

--- src/main/java/com/rocketden/main/game_object/Player.java
+++ src/main/java/com/rocketden/main/game_object/Player.java
@@ -1,5 +1,6 @@
 package com.rocketden.main.game_object;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import com.rocketden.main.model.User;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Player {
 
     // User associated with this Player object.
--- src/main/java/com/rocketden/main/game_object/PlayerCode.java
+++ src/main/java/com/rocketden/main/game_object/PlayerCode.java
@@ -1,15 +1,20 @@
 package com.rocketden.main.game_object;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public class PlayerCode {
 
-    // Updated field to hold the user's current-language code.
     private String code;
-
     private String language;
+
+    public PlayerCode(String code, String language) {
+        this.code = code;
+        this.language = language;
+    }
 
 }
