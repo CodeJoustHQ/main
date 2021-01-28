d26955a6bc7f884833658ccfe8f5cc51296b45f9
Chris Elliott <elliott77chris@gmail.com>
2021-01-28 14:27:53 -0800

    Implement ProblemIOType file.

--- src/main/java/com/rocketden/main/model/problem/ProblemIOType.java
+++ src/main/java/com/rocketden/main/model/problem/ProblemIOType.java
@@ -1,5 +1,42 @@
-package com.rocketden.main.model.problem;
+package com.rocketden.tester.model.problem;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.rocketden.tester.exception.ProblemError;
+import com.rocketden.tester.exception.api.ApiException;
+
+import lombok.Getter;
+
+@Getter
 public enum ProblemIOType {
-	STRING, STRING_LIST, STRING_DICTIONARY, INTEGER, INTEGER_LIST, BOOLEAN, BOOLEAN_LIST, VOID, CUSTOM, CUSTOM_LIST
+    
+    STRING(String.class),
+    INTEGER(Integer.class),
+    DOUBLE(Double.class),
+    CHARACTER(Character.class),
+    BOOLEAN(Boolean.class),
+    ARRAY_STRING(String[].class),
+    ARRAY_INTEGER(Integer[].class),
+    ARRAY_DOUBLE(Double[].class),
+    ARRAY_CHARACTER(Character[].class),
+    ARRAY_BOOLEAN(Boolean[].class);
+
+    private final Class<?> classType;
+
+    ProblemIOType(Class<?> classType) {
+        this.classType = classType;
+    }
+
+    // Convert a matching string (ignoring case) to enum object
+    @JsonCreator
+    public static ProblemIOType fromString(String value) {
+        try {
+            return ProblemIOType.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new ApiException(ProblemError.BAD_IOTYPE);
+        }
+    }
+
+    public boolean typeMatches(Object value) {
+        return value.getClass().equals(this.getClassType());
+    }
 }
