2f6c73914671f1b84f527d6b39e0375e1f48876b
Chris Elliott <elliott77chris@gmail.com>
2021-01-29 16:52:24 -0800

    Fix toObjectLocalDateTime function by appending any number of microseconds.

--- src/test/java/com/rocketden/main/util/UtilityTestMethods.java
+++ src/test/java/com/rocketden/main/util/UtilityTestMethods.java
@@ -3,6 +3,8 @@ package com.rocketden.main.util;
 import java.lang.reflect.Type;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -12,6 +14,8 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 
 public class UtilityTestMethods {
+
+    private final static String DATE_TIME_FORMAT_PATTERN = "yyyy-MM-dd'T'HH:mm:ss";
 
     public static String convertObjectToJsonString(Object o) {
         Gson gson = new Gson();
@@ -33,9 +37,15 @@ public class UtilityTestMethods {
         Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class,
             new JsonDeserializer<LocalDateTime>() { 
             @Override 
-            public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException { 
-                return LocalDateTime.parse(json.getAsString(),
-                    DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS"));
+            public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+                    DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                        .appendPattern(DATE_TIME_FORMAT_PATTERN)
+                        // optional decimal point followed by 1 to 6 digits
+                        .optionalStart()
+                        .appendFraction(ChronoField.MICRO_OF_SECOND, 1, 6, true)
+                        .optionalEnd()
+                        .toFormatter(); 
+                    return LocalDateTime.parse(json.getAsString(), formatter);
                 } 
             }).create();
         return gson.fromJson(json, c);
