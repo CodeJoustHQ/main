32fa78076f22d19a034fdd234d718a14d73482e3
Alan Bi <alan.bi326@gmail.com>
2021-02-06 00:56:07 -0800

    Implement basic show all problems page

---
+++ frontend/src/components/card/ProblemCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProblemCard() {
+  return (
+    <div>
+      stuff
+    </div>
+  );
+}
+
+export default ProblemCard;
--- frontend/src/views/AllProblemsPage.tsx
+++ frontend/src/views/AllProblemsPage.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getProblems, Problem } from '../api/Problem';
+import { LargeText } from '../components/core/Text';
+import ErrorMessage from '../components/core/Error';
+import Loading from '../components/core/Loading';
+import ProblemCard from '../components/card/ProblemCard';
 
 function AllProblemsPage() {
+  const [problems, setProblems] = useState<Problem[] | null>(null);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getProblems()
+      .then((res) => {
+        setProblems(res);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
-      stuff
+      <LargeText>View All Problems</LargeText>
+      { error ? <ErrorMessage message={error} /> : null }
+      { loading ? <Loading /> : null }
+
+      {problems?.forEach((problem) =>
+        <ProblemCard problem={problem} />
+      )}
     </div>
   );
 }
