dcb9582ecea5e44d622e675dc1307ea6f5e7a110
Alan Bi <alan.bi326@gmail.com>
2021-02-13 10:12:01 -0800

    Update TesterResponse with new values

--- src/main/java/com/rocketden/main/dto/game/TesterResponse.java
+++ src/main/java/com/rocketden/main/dto/game/TesterResponse.java
@@ -3,10 +3,24 @@ package com.rocketden.main.dto.game;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 // This class is mapped directly to the tester service's RunDto class.
 public class TesterResponse {
-    private Boolean status;
-    private String output;
+    private List<TesterResultResponse> results;
+    private Integer numCorrect;
+    private Integer numTestCases;
+    private Long runtime;
+
+    @Getter
+    @Setter
+    public static class TesterResultResponse {
+        private String console;
+        private String userOutput;
+        private String error;
+        private String correctOutput;
+        private boolean correct;
+    }
 }
