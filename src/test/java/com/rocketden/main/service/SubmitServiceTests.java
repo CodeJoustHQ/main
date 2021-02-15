ab1917493ed9d69c778743bab2ba6ea57383091f
Alan Bi <alan.bi326@gmail.com>
2021-02-15 13:34:34 -0800

    Update tests to more rigorously cover submissions

--- src/test/java/com/rocketden/main/service/SubmitServiceTests.java
+++ src/test/java/com/rocketden/main/service/SubmitServiceTests.java
@@ -3,9 +3,13 @@ package com.rocketden.main.service;
 import com.rocketden.main.dto.game.GameMapper;
 import com.rocketden.main.dto.game.SubmissionRequest;
 import com.rocketden.main.dto.game.TesterRequest;
+import com.rocketden.main.dto.game.TesterResponse;
+import com.rocketden.main.dto.problem.ProblemTestCaseDto;
 import com.rocketden.main.dto.user.UserMapper;
 import com.rocketden.main.dto.problem.ProblemDto;
 import com.rocketden.main.exception.GameError;
+import com.rocketden.main.exception.TesterError;
+import com.rocketden.main.exception.api.ApiErrorResponse;
 import com.rocketden.main.exception.api.ApiException;
 import com.rocketden.main.game_object.CodeLanguage;
 import com.rocketden.main.game_object.Game;
@@ -16,10 +20,13 @@ import com.rocketden.main.model.problem.Problem;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,6 +34,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class SubmitServiceTests {
@@ -115,44 +124,113 @@ public class SubmitServiceTests {
     }
 
     @Test
-    public void callTesterServiceReturnsDummyResponse() {
+    public void callTesterServiceReturnsDummyResponse() throws Exception {
+        ProblemTestCaseDto testCaseDto = new ProblemTestCaseDto();
+        ProblemDto problemDto = new ProblemDto();
+        problemDto.setTestCases(Collections.singletonList(testCaseDto));
+
         TesterRequest request = new TesterRequest();
         request.setCode(CODE);
         request.setLanguage(LANGUAGE);
-        request.setProblem(new ProblemDto());
+        request.setProblem(problemDto);
 
         Submission response = submitService.getSubmission(request);
 
         assertNotNull(response);
+        verify(submitService, never()).callTesterService(Mockito.any());
     }
 
     @Test
-    public void callTesterServiceSuccessfulApiCall() {
+    public void callTesterServiceSuccessfulApiCall() throws Exception {
         submitService.setDebugModeForTesting(false);
 
+        ProblemTestCaseDto testCaseDto = new ProblemTestCaseDto();
+        ProblemDto problemDto = new ProblemDto();
+        problemDto.setTestCases(Collections.singletonList(testCaseDto));
+
+        TesterRequest request = new TesterRequest();
+        request.setCode(CODE);
+        request.setLanguage(LANGUAGE);
+        request.setProblem(problemDto);
+
+        TesterResponse testerResponse = new TesterResponse();
+        testerResponse.setNumCorrect(1);
+        testerResponse.setNumTestCases(1);
+        testerResponse.setRuntime(5.5);
+
+        Mockito.doReturn(testerResponse).when(submitService).callTesterService(request);
+        Submission response = submitService.getSubmission(request);
+
+        assertEquals(testerResponse.getNumCorrect(), response.getNumCorrect());
+        assertEquals(testerResponse.getNumTestCases(), response.getNumTestCases());
+        assertEquals(testerResponse.getRuntime(), response.getRuntime());
+        assertEquals(request.getCode(), response.getPlayerCode().getCode());
+        assertEquals(request.getLanguage(), response.getPlayerCode().getLanguage());
+        assertNotNull(response.getStartTime());
     }
 
     @Test
-    public void callTesterServiceTesterThrowsError() {
+    public void callTesterServiceTesterThrowsError() throws Exception {
         submitService.setDebugModeForTesting(false);
 
+        ProblemTestCaseDto testCaseDto = new ProblemTestCaseDto();
+        ProblemDto problemDto = new ProblemDto();
+        problemDto.setTestCases(Collections.singletonList(testCaseDto));
+
+        TesterRequest request = new TesterRequest();
+        request.setCode(CODE);
+        request.setLanguage(LANGUAGE);
+        request.setProblem(problemDto);
+
+        TesterResponse testerResponse = new TesterResponse();
+        testerResponse.setNumCorrect(1);
+        testerResponse.setNumTestCases(1);
+        testerResponse.setRuntime(5.5);
+
+        TesterError ERROR = new TesterError(HttpStatus.BAD_REQUEST, new ApiErrorResponse("Bad input", "INVALID_INPUT"));
+
+        Mockito.doThrow(new ApiException(ERROR)).when(submitService).callTesterService(request);
+
+        ApiException exception = assertThrows(ApiException.class, () -> submitService.getSubmission(request));
+
+        assertEquals(ERROR, exception.getError());
     }
 
     @Test
-    public void callTesterServiceInternalError() {
+    public void callTesterServiceInternalError() throws Exception {
         submitService.setDebugModeForTesting(false);
 
+        ProblemTestCaseDto testCaseDto = new ProblemTestCaseDto();
+        ProblemDto problemDto = new ProblemDto();
+        problemDto.setTestCases(Collections.singletonList(testCaseDto));
+
+        TesterRequest request = new TesterRequest();
+        request.setCode(CODE);
+        request.setLanguage(LANGUAGE);
+        request.setProblem(problemDto);
+
+        TesterResponse testerResponse = new TesterResponse();
+        testerResponse.setNumCorrect(1);
+        testerResponse.setNumTestCases(1);
+        testerResponse.setRuntime(5.5);
+
+        Mockito.doThrow(new Exception()).when(submitService).callTesterService(request);
+
+        ApiException exception = assertThrows(ApiException.class, () -> submitService.getSubmission(request));
+
+        assertEquals(GameError.TESTER_ERROR, exception.getError());
     }
 
     @Test
-    public void callTesterServiceFailsNoDebug() {
+    public void callTesterServiceErrorWithPostRequest() {
         submitService.setDebugModeForTesting(false);
 
         TesterRequest request = new TesterRequest();
         request.setCode("temp");
 
-        ApiException exception = assertThrows(ApiException.class, () -> submitService.callTesterService(request));
+        assertThrows(Exception.class, () -> submitService.callTesterService(request));
 
+        ApiException exception = assertThrows(ApiException.class, () -> submitService.getSubmission(request));
         assertEquals(GameError.TESTER_ERROR, exception.getError());
     }
 }
