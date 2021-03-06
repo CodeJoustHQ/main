2615773f3100d1354cc8b6a03026c3ca3dcb422b
Chris Elliott <elliott77chris@gmail.com>
2021-03-05 23:23:04 -0800

    Fix memory leak by moving the floating circle styling to attributes.

--- frontend/src/components/special/FloatingCircle.tsx
+++ frontend/src/components/special/FloatingCircle.tsx
@@ -19,17 +19,22 @@ export type CircleHeaderParams = {
   size: number
 }
 
-export const FloatingCircle = styled.div<CircleParams>`
+const movementReduction: number = 40;
+
+export const FloatingCircle = styled.div.attrs((props: CircleParams) => ({
+  style: {
+    background: props.color,
+    bottom: `${props.bottom}%`,
+    left: `${props.left}%`,
+    width: `${props.size}rem`,
+    height: `${props.size}rem`,
+    transform: `translate(${props.x / movementReduction}px, ${props.y / movementReduction}px)`,
+  },
+}))<CircleParams>`
   position: absolute;
-  bottom: ${({ bottom }) => bottom}%;
-  left: ${({ left }) => left}%;
-  width: ${({ size }) => size}rem;
-  height: ${({ size }) => size}rem;
-  background: ${({ color }) => color};
   border-radius: 50%;
   transition: transform 250ms, opacity 400ms;
   transition: left 0.25s ease;
-  transform: translate(${({ x }) => x}px, ${({ y }) => y}px);
 `;
 
 export const FloatingCircleHeader = styled.div<CircleParams>`
--- frontend/src/views/Landing.tsx
+++ frontend/src/views/Landing.tsx
@@ -7,7 +7,6 @@ import { FloatingCircle, Coordinate } from '../components/special/FloatingCircle
 
 function LandingPage() {
   const [mousePosition, setMousePosition] = useState<Coordinate>({ x: 0, y: 0 });
-  const movementReduction: number = 40;
 
   const mouseMoveHandler = useCallback((e: MouseEvent) => {
     setMousePosition({ x: e.clientX, y: e.clientY });
@@ -21,40 +20,40 @@ function LandingPage() {
     <LandingPageContainer>
       <FloatingCircle
         color={ThemeConfig.colors.redCircle}
-        x={mousePosition.x / movementReduction}
-        y={mousePosition.y / movementReduction}
+        x={mousePosition.x}
+        y={mousePosition.y}
         bottom={50}
         left={-30}
         size={4}
       />
       <FloatingCircle
         color={ThemeConfig.colors.greenCircle}
-        x={mousePosition.x / movementReduction}
-        y={mousePosition.y / movementReduction}
+        x={mousePosition.x}
+        y={mousePosition.y}
         bottom={-30}
         left={-20}
         size={8}
       />
       <FloatingCircle
         color={ThemeConfig.colors.yellowCircle}
-        x={mousePosition.x / movementReduction}
-        y={mousePosition.y / movementReduction}
+        x={mousePosition.x}
+        y={mousePosition.y}
         bottom={-20}
         left={47.5}
         size={2}
       />
       <FloatingCircle
         color={ThemeConfig.colors.pinkCircle}
-        x={mousePosition.x / movementReduction}
-        y={mousePosition.y / movementReduction}
+        x={mousePosition.x}
+        y={mousePosition.y}
         bottom={-25}
         left={95}
         size={7}
       />
       <FloatingCircle
         color={ThemeConfig.colors.blueCircle}
-        x={mousePosition.x / movementReduction}
-        y={mousePosition.y / movementReduction}
+        x={mousePosition.x}
+        y={mousePosition.y}
         bottom={100}
         left={90}
         size={4}
