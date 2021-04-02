065febe2d72c764d8f5eb1cbcc7841dd64c260a0
Chris Elliott <elliott77chris@gmail.com>
2021-04-02 15:06:21 -0700

    Add the lock screen for the create problem page.

--- frontend/src/views/CreateProblemPage.tsx
+++ frontend/src/views/CreateProblemPage.tsx
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { useHistory } from 'react-router-dom';
-import { createProblem, Problem, ProblemIOType } from '../api/Problem';
+import { useHistory, useLocation } from 'react-router-dom';
+import {
+  accessProblems,
+  createProblem,
+  Problem,
+  ProblemIOType,
+} from '../api/Problem';
 import { LargeText } from '../components/core/Text';
 import ErrorMessage from '../components/core/Error';
 import Loading from '../components/core/Loading';
 import ProblemDisplay from '../components/problem/ProblemDisplay';
 import { Difficulty } from '../api/Difficulty';
+import { checkLocationState } from '../util/Utility';
+import LockScreen from '../components/core/LockScreen';
+
+type LocationState = {
+  locked: boolean,
+};
 
 const Content = styled.div`
   display: flex;
@@ -24,9 +35,40 @@ function CreateProblemPage() {
   };
 
   const history = useHistory();
+  const location = useLocation<LocationState>();
   const [problem, setProblem] = useState<Problem>(firstProblem);
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
+
+  // The problems page is locked until a valid password is supplied.
+  const [locked, setLocked] = useState(true);
+
+  useEffect(() => {
+    if (checkLocationState(location, 'locked')) {
+      setLocked(location.state.locked);
+    }
+  }, [location]);
+
+  const sendAccessCreateProblem = (passwordParam: string) => {
+    setLoading(true);
+    setError('');
+    accessProblems(passwordParam)
+      .then((access: boolean) => {
+        setLoading(false);
+        if (access) {
+          // Push to history to give access with location on refresh.
+          history.push('/problem/create', {
+            locked: false,
+          });
+        } else {
+          setError('The password was incorrect; please contact support@codejoust.co if you wish to help edit problems.');
+        }
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err.message);
+      });
+  };
 
   const handleSubmit = (newProblem: Problem) => {
     setLoading(true);
@@ -46,14 +88,22 @@ function CreateProblemPage() {
   };
 
   return (
-    <>
-      <LargeText>Create Problem</LargeText>
-      { error ? <ErrorMessage message={error} /> : null }
-      { loading ? <Loading /> : null }
-      <Content>
-        <ProblemDisplay problem={problem!} onClick={handleSubmit} actionText="Create" editMode={false} />
-      </Content>
-    </>
+    locked ? (
+      <LockScreen
+        loading={loading}
+        error={error}
+        enterPasswordAction={sendAccessCreateProblem}
+      />
+    ) : (
+      <>
+        <LargeText>Create Problem</LargeText>
+        { error ? <ErrorMessage message={error} /> : null }
+        { loading ? <Loading /> : null }
+        <Content>
+          <ProblemDisplay problem={problem!} onClick={handleSubmit} actionText="Create" editMode={false} />
+        </Content>
+      </>
+    )
   );
 }
 
