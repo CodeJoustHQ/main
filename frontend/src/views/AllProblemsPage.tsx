35f1666753ef6df36c1d9b9540f7cf88618283c8
Chris Elliott <elliott77chris@gmail.com>
2021-04-02 10:21:06 -0700

    Add initial password setup for problems page.

--- frontend/src/views/AllProblemsPage.tsx
+++ frontend/src/views/AllProblemsPage.tsx
@@ -18,6 +18,13 @@ function AllProblemsPage() {
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(true);
 
+  // Variable to hold whether the user is focused on the text input field.
+  const [focusInput, setFocusInput] = useState(false);
+
+  // The problems page is locked until a valid password is supplied.
+  const [locked, setLocked] = useState(true);
+  const [password, setPassword] = useState('');
+
   useEffect(() => {
     getProblems()
       .then((res) => {
@@ -35,20 +42,53 @@ function AllProblemsPage() {
   };
 
   return (
-    <Content>
-      <LargeText>View All Problems</LargeText>
-      <TextLink to="/problem/create">Create new problem</TextLink>
-      { error ? <ErrorMessage message={error} /> : null }
-      { loading ? <Loading /> : null }
-
-      {problems?.map((problem, index) => (
-        <ProblemCard
-          key={index}
-          problem={problem}
-          onClick={redirect}
+    locked ? (
+      <Content>
+        <LargeText>
+          What is the password?
+        </LargeText>
+        <LargeCenterInputText
+          placeholder="Your nickname"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setPassword(event.target.value);
+          }}
+          onFocus={() => {
+            setFocusInput(true);
+          }}
+          onBlur={() => {
+            setFocusInput(false);
+          }}
+          onKeyPress={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setError('');
+            if (event.key === 'Enter') {
+              alert(event.target.value);
+            }
+          }}
         />
-      ))}
-    </Content>
+        <PrimaryInput
+          onClick={() => {
+            alert(password);
+          }}
+          value="Enter"
+          disabled={!password}
+        />
+      </Content>
+    ) : (
+      <Content>
+        <LargeText>View All Problems</LargeText>
+        <TextLink to="/problem/create">Create new problem</TextLink>
+        { error ? <ErrorMessage message={error} /> : null }
+        { loading ? <Loading /> : null }
+
+        {problems?.map((problem, index) => (
+          <ProblemCard
+            key={index}
+            problem={problem}
+            onClick={redirect}
+          />
+        ))}
+      </Content>
+    )
   );
 }
 
