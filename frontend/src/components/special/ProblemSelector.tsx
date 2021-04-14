b4bb7206864d1478c4a64c7a10e0b8d76336cea9
Alan Bi <alan.bi326@gmail.com>
2021-04-14 01:04:15 -0700

    Toggle showing list of problems

--- frontend/src/components/special/ProblemSelector.tsx
+++ frontend/src/components/special/ProblemSelector.tsx
@@ -5,12 +5,24 @@ import ErrorMessage from '../core/Error';
 import { displayNameFromDifficulty } from '../../api/Difficulty';
 import { DifficultyDisplayButton } from '../core/Button';
 
+type ContentProps = {
+  show: boolean,
+};
+
+const InnerContent = styled.div<ContentProps>`
+  display: ${({ show }) => (show ? 'block' : 'none')};
+  border-radius: 5px;
+`;
+
 const InlineProblem = styled.div`
   width: 300px;
   height: 50px;
   display: flex;
   flex: auto;
   justify-content: space-between;
+  background-color: ${({ theme }) => theme.colors.white};
+  
+  border: solid 1px ${({ theme }) => theme.colors.gray};
 `;
 
 const ProblemName = styled.p`
@@ -20,6 +32,7 @@ const ProblemName = styled.p`
 function ProblemSelector() {
   const [error, setError] = useState<string>('');
   const [problems, setProblems] = useState<SelectableProblem[]>([]);
+  const [showProblems, setShowProblems] = useState(false);
 
   useEffect(() => {
     getProblems()
@@ -33,20 +46,23 @@ function ProblemSelector() {
 
   return (
     <div>
-      {problems.map((problem) => (
-        <InlineProblem key={problem.name}>
-          <ProblemName>
-            {problem.name}
-          </ProblemName>
-          <DifficultyDisplayButton
-            difficulty={problem.difficulty}
-            enabled={false}
-            active
-          >
-            {displayNameFromDifficulty(problem.difficulty)}
-          </DifficultyDisplayButton>
-        </InlineProblem>
-      ))}
+      <button onClick={() => setShowProblems(!showProblems)}>Select a problem...</button>
+      <InnerContent show={showProblems}>
+        {problems.map((problem) => (
+          <InlineProblem key={problem.name}>
+            <ProblemName>
+              {problem.name}
+            </ProblemName>
+            <DifficultyDisplayButton
+              difficulty={problem.difficulty}
+              enabled={false}
+              active
+            >
+              {displayNameFromDifficulty(problem.difficulty)}
+            </DifficultyDisplayButton>
+          </InlineProblem>
+        ))}
+      </InnerContent>
 
       { error ? <ErrorMessage message={error} /> : null }
     </div>
